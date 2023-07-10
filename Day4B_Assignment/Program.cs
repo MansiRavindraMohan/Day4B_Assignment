c8ea44cb46b8dd9accb3d6642f624c0287009e23
MansiRavindraMohan <manasimohan29@gmail.com>
2023-07-10 14:39:49 +0530

    [Mansi]: Program to count a total number of duplicate elements in an array

---
+++ Day4B_Assignment/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4B_Assignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayCount arrayCount = new ArrayCount();
+            arrayCount.MethodArrayCount();
+            Console.ReadLine();
+        }
+    }
+}
